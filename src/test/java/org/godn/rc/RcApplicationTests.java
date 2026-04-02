3063057c3762f24a09a8096b36f4f1885a38c5a9
GoDn76 <godncraft7610tube@gmail.com>
2026-04-03 00:49:09 +0530

    First Commit Starting the Real Time Chat Application

---
+++ src/test/java/org/godn/rc/RcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.godn.rc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
